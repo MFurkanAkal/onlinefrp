88c03b8c73fa67e368837476918ac7b12d585ec8
Furkan Akal <30433306+furkandev@users.noreply.github.com>
2018-02-04 12:01:04 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -9,13 +9,25 @@
 	$isim = @$_POST["isim"];
 	$deger = @$_POST["deger"];
 	$gönder = @$_POST["gönder"];
+	$temizle = @$_POST["temizle"];
+
+	if($temizle){
+	$ac2 = @fopen("chat.txt", "w");
+	$icerik2 = "";
+	fwrite($ac2,$icerik2);
+	fclose($ac2);
+	}
 
 	if($gönder){
+	if($isim == "" || $deger == ""){
+		echo "Boş bırakmayınız..";
+	}	else {
 	$ac = @fopen("chat.txt", "a");
 	$icerik = "$isim: \n $deger </br>";
 	fwrite($ac,$icerik);
 	fclose($ac);
 	}
+  }
 
 	?>
 	MESAJLAR;
@@ -30,8 +42,8 @@
 			<input type = "text" name = "isim"/> <br /> <br />
 			Mesaj <br />
 			<textarea name = "deger" id = "" cols= "30" rows = "10"></textarea> <br /> <br />
-			<input type="submit" name="gönder"/>
-		</form
+			<input type="submit" name="gönder"/><input type="submit" value="Temizle" name="temizle"/>
+		</form>	
 	</div>
 </body>
 </html>
